4d8633bb24be3fdf417702613dc4c4f0f2b902ce
Mohamed Ouattara <mohamedd.ouattara@gmail.com>
2023-01-14 16:20:23 +0000

    Reset all

--- Scripts/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-  var byeSpeaker = {};
-  var speakWord = "Good Bye ";
-  byeSpeaker.speak = function(){
-    console.log(speakWord+names[i]);
-  }
-  window.byeSpeaker = byeSpeaker;
-})(window);
--- Scripts/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-  var helloSpeaker = {};
-  var speakWord = "Hello ";
-  helloSpeaker.speak = function() {
-    console.log(speakWord+names[i]);
-  }
-  window.helloSpeaker = helloSpeaker;
-})(window);
