79a410dd0d8a803aad2c9a5aad90c6def13226c6
Mohamed Ouattara <mohamedd.ouattara@gmail.com>
2023-01-15 16:41:39 +0000

    Udpate

--- public/script.js
+++ public/script.js
@@ -1,9 +1,23 @@
 let blocPrincipal = document.querySelector('.blocPrincipal');
-console.log(blocPrincipal);
+let mainParent = document.querySelectorAll('.mainParent');
+let taskCard = document.querySelectorAll('.taskCard');
+let btnDelete = document.querySelectorAll('.btnDelete');
 
 
-let taskCard = document.querySelector('.taskCard')
-console.log(taskCard);
+taskCard.forEach((task) => {
+    task.addEventListener('click', function(e) {
+        if (e.target.className == 'btnDelete px-5 material-symbols-outlined') {
+            let parent = e.target.parentElement.parentElement.parentElement.parentElement;
+            let child = e.target.parentElement.parentElement.parentElement;
+            parent.removeChild(child)
+            // console.log(parent, 'Good');
+            // console.log(blocPrincipal.length);
+        }
+    })
+})
+console.log(blocPrincipal.innerText);
 
-let btnDelete = document.querySelector('.btnDelete')
-console.log(btnDelete);
+if (blocPrincipal.innerText < 'Task name delete') {
+    console.log(blocPrincipal);
+    document.querySelector('.isolate').removeChild(blocPrincipal);
+}
