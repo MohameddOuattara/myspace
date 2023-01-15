b6b100b39a955fced6c9f459df74b7fab7bc3c43
Mohamed Ouattara <mohamedd.ouattara@gmail.com>
2023-01-15 11:49:29 +0000

    Adding new HTML file for deploying

--- public/script.js
+++ public/script.js
@@ -1,10 +1,6 @@
 let blocPrincipal = document.querySelector('.blocPrincipal');
 console.log(blocPrincipal);
-blocPrincipal.addEventListener('click', function(e){
-    if (e.target.className.include == 'btnDelete') {
-        console.log('Ok');
-    }
-})
+
 
 let taskCard = document.querySelector('.taskCard')
 console.log(taskCard);
